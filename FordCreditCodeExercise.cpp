81ef786201d58abab55d129766d0af2c548a7d33
tleminh2014 <74930159+tleminh2014@users.noreply.github.com>
2021-03-13 14:57:19 -0500

    Adding expected results to the test cases

--- FordCreditCodeExercise.cpp
+++ FordCreditCodeExercise.cpp
@@ -1,4 +1,5 @@
-// FordCreditCodeExercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Ford Credit Code exercise 1 - Trang Leminh 
+//FordCreditCodeExercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -51,14 +52,15 @@ void calcDist(string line1,string line2)
     }
 }
 
-
+//The main function will test five use cases with differing parameters
 
 int main()
 { 
     string lineA = "(-1, 2)";
     string lineB = "(7, -2)";
     cout << "********************Test1: Testing single digit coordinates with spaces*****************************\n"
-        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << "" <<endl;
+        << "First coordinate is: " << lineA << " second coordinate is: " << lineB <<endl;
+    cout << "The calculated distance should be: 12\n";
     calcDist(lineA, lineB);
     cout << "*************************************End of Test1****************************************\n\n\n";
 
@@ -66,7 +68,8 @@ int main()
     lineA = "(-10,90)";
     lineB = "(80, -40)";
     cout << "********************Test2: Testing double digit coordinates with no spaces AND spaces*****************************\n"
-        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << "" << endl;
+        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << endl;
+    cout << "The calculated distance should be: 220\n";
     calcDist(lineA, lineB);
     cout << "*************************************End of Test2****************************************\n\n\n";
 
@@ -74,14 +77,16 @@ int main()
     lineA = "(,)";
     lineB = "(,)";
     cout << "********************Test3: Testing empty coordinates*****************************\n"
-        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << "" << endl;
+        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << endl;
+    cout << "There should be an invalid errror output\n";
     calcDist(lineA, lineB);
     cout<< "*************************************End of Test3****************************************\n\n\n";
 
     lineA = "(a,b)";
     lineB = "(c,d)";
     cout << "********************Test4: Testing non-digit coordinates*****************************\n"
-        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << "" << endl;
+        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << endl;
+    cout << "There should be an invalid errror output\n";
     calcDist(lineA, lineB);
     cout << "*************************************End of Test4****************************************\n\n\n";
 
@@ -89,7 +94,8 @@ int main()
     lineA = "(10,b)";
     lineB = "(c,5)";
     cout << "********************Test5: Testing non-digit and numbers coordinates*****************************\n"
-        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << "" << endl;
+        << "First coordinate is: " << lineA << " second coordinate is: " << lineB << endl;
+    cout << "There should be an invalid errror output\n";
     calcDist(lineA, lineB);
     cout << "*************************************End of Test5****************************************\n\n\n";
 
